9e1cfa0e32b742bc9c1a9c310a1eb6c3f300a267
ManuGolem <manuelcarreras2013@gmail.com>
2025-02-19 12:00:01 -0300

    change the text of the button with the change of the class

--- app.js
+++ app.js
@@ -1,18 +1,25 @@
-const toggleDark=document.querySelector("#toggle-dark");
-const body=document.querySelector("body");
+const toggleDark = document.querySelector("#toggle-dark");
+const textoBoton = toggleDark.querySelector("span");
+const body = document.querySelector("body");
 
-document.addEventListener("DOMContentLoaded",()=>{
+document.addEventListener("DOMContentLoaded", () => {
     actualizarMode();
-    toggleDark.addEventListener("click",()=>{
-        
-    })
-});
-function actualizarMode(){
-    const darkMode=window.matchMedia("(prefers-color-scheme:dark)").matches;
-    if(darkMode){
+    toggleDark.addEventListener("click", () => {
         body.classList.toggle("darkMode");
-
-    }else{
-        body.classList.toggle("darkMode")
-    }    
+        if (body.classList.contains("darkMode")) {
+            textoBoton.textContent = "Light Mode";
+        } else {
+            textoBoton.textContent = "Dark Mode";
+        }
+    });
+});
+function actualizarMode() {
+    const darkMode = window.matchMedia("(prefers-color-scheme:dark)").matches;
+    if (darkMode) {
+        body.classList.toggle("darkMode");
+        textoBoton.textContent = "Light Mode";
+    } else {
+        body.classList.toggle("darkMode");
+        textoBoton.textContent = "Dark Mode";
+    }
 }
